9dfc5068e9c4a2134fa86a93b5d70224e502b4c5
Rheyno <Rheynoair@gmail.com>
2025-11-13 18:42:43 +0700

    Update layout.jsx

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -6,7 +6,7 @@ export default function LayoutPage() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 overflow-y-auto min-w-0 p-4">
+      <main className="flex-1 overflow-y-auto min-w-0 ">
         <Outlet />
       </main>
       <Footer />
