8933db22c2ba3f62b0e225a8a0511a398934763f
Rheyno <Rheynoair@gmail.com>
2025-11-13 18:29:55 +0700

    Update layout.jsx

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,14 +1,15 @@
-import { Footer } from "../component/Footer";
-import { Header } from "./header";
+import { Footer } from "../components/Footer";
+import { Header } from "./components/Header";
 import { Outlet } from "react-router-dom";
-export function LayoutPage() {
+
+export default function LayoutPage() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 overflow-y-auto min-w-0 ">
-<Outlet />
-</main>
+      <main className="flex-1 overflow-y-auto min-w-0 p-4">
+        <Outlet />
+      </main>
       <Footer />
-    </>
+    </div>
   );
 }
